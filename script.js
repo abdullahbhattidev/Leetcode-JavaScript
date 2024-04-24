ddb7088cdadba1dc7b0b37b00c63618cd487ab90
abdullahbhattidev <ab76293@gmail.com>
2024-04-25 01:11:19 +0500

    Problem 1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log ("hello world!")
+//Problem 1. Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+const f = createHelloWorld();
+ f(); 
+ console.log(f())
+ 
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
