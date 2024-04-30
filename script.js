6f43526df668e2b328ce4344e7ef93745c6fbada
abdullahbhattidev <ab76293@gmail.com>
2024-04-30 23:42:48 +0500

    Problem 4

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-//Problem 3. Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+//Problem 4. Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
 
-// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
-// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
 /**
- * @param {string} val
- * @return {Object}
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
  */
-var expect = function(val) {
-    return{
-        toBe: (value)=>{
-            if(value===val)
-            return true
-            else throw new Error("Not Equal")
+var createCounter = function(init) {
+    let current = init;
+    return {
+        increment:() => {
+           return ++init
         },
-        notToBe: (value)=>{
-            if(value!==val)
-            return true
-            else throw new Error("Equal")
+        reset: ()=> {
+            return init = current 
+        },
+        decrement:()=> {
+            return --init
         }
+        
     }
 };
 
 /**
- * expect(5).toBe(5); // true
- * expect(5).notToBe(5); // throws "Equal"
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
  */
